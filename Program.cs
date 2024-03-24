b2e078295712912f0a126a872d26ed8d08bc4dda
hwangminghung <hwangminghung@gmail.com>
2024-03-24 15:46:44 +0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Nhap kich thuoc cua mang:");
+        int size = int.Parse(Console.ReadLine());
+
+        int[] array = new int[size];
+
+        Console.WriteLine("Nhap cac phan tu cua mang:");
+
+        // Nhap gia tri cho tung phan tu cua mang
+        for (int i = 0; i < size; i++)
+        {
+            Console.WriteLine($"Nhap gia tri cho phan tu thu {i}:");
+            array[i] = int.Parse(Console.ReadLine());
+        }
+
+        int minValue = FindMinValue(array);
+
+        Console.WriteLine($"Gia tri nho nhat trong mang la {minValue}.");
+    }
+
+    static int FindMinValue(int[] array)
+    {
+        int minValue = array[0];
+
+        // Tim gia tri nho nhat trong mang
+        for (int i = 1; i < array.Length; i++)
+        {
+            if (array[i] < minValue)
+            {
+                minValue = array[i];
+            }
+        }
+
+        return minValue;
+    }
+}
